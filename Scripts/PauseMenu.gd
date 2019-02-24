cf3f12399a2be91f19f0c7019e4be3b925288883
TheCobaye54 <florian.dehe@gmail.com>
2019-02-24 17:03:18 +0100

    menus

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
-func _on_Resume_pressed():
-	print("marche")
-	get_parent().get_parent().destroy_menu()
-
-
 func _on_Quit_pressed():
-	print("quit")
 	get_tree().quit()
 
 
 func _on_Options_pressed():
 	pass # replace with function body
 
+
+func _on_Resume_pressed():
+	pass # replace with function body
