8e6d1817935b4ae8ced248b00dbf43df7ff168cc
TheCobaye54 <florian.dehe@gmail.com>
2019-02-24 18:44:22 +0100

    main menu ok

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,8 +1,16 @@
 extends CanvasLayer
 
+var tuto_image
+
+func _ready():
+	tuto_image = get_node("TextureRect2")
+
 func play():
 	get_tree().change_scene("res://World.tscn")
-
+	
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		play()
+	if Input.is_action_just_pressed("tuto"):
+		tuto_image.visible = not tuto_image.visible
+
+func _on_quit_pressed():
+	get_tree().quit()
